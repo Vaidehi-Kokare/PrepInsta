b4205d5b3f69057c54d85e2932f51261353a74f2
Vaidehi-Kokare <vaidehikokare@gmail.com>
2023-01-08 12:27:20 +0530

    Added: Checking if number is positive or negative

---
+++ src/main/java/com/bridgelabz/prepinsta/PositiveNegative.java
@@ -0,0 +1,22 @@
+package com.bridgelabz.prepinsta;
+
+
+import java.util.Scanner;
+
+public class PositiveNegative {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter number");
+        int num = scanner.nextInt();
+        if(num>0)
+            System.out.println("number is positive");
+        else
+            if(num<0)
+            System.out.println("Number is negative");
+            else
+            System.out.println("Number is zero");
+
+
+
+    }
+}
